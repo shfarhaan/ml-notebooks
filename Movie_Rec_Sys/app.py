f8f4da6f7aa1a8d44069660db3b1cd19c507c645
farhaan-sh <shf.cse@gmail.com>
2024-02-04 22:03:32 +0600

    recommendation system update

--- Movie_Rec_Sys/app.py
+++ Movie_Rec_Sys/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import requests
+import streamlit.components.v1 as components
 
 def fetch_poster(movie_id):
      url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
@@ -16,29 +17,6 @@ movies_list=movies['title'].values
 
 st.header("Movie Recommender System")
 
-import streamlit.components.v1 as components
-
-imageCarouselComponent = components.declare_component("image-carousel-component", path="")
-
-
-imageUrls = [
-    fetch_poster(1632),
-    fetch_poster(299536),
-    fetch_poster(17455),
-    fetch_poster(2830),
-    fetch_poster(429422),
-    fetch_poster(9722),
-    fetch_poster(13972),
-    fetch_poster(240),
-    fetch_poster(155),
-    fetch_poster(598),
-    fetch_poster(914),
-    fetch_poster(255709),
-    fetch_poster(572154)
-    ]
-
-
-imageCarouselComponent(imageUrls=imageUrls, height=200)
 selectvalue=st.selectbox("Select movie from dropdown", movies_list)
 
 def recommend(movie):
@@ -46,7 +24,7 @@ def recommend(movie):
     distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
     recommend_movie=[]
     recommend_poster=[]
-    for i in distance[1:6]:
+    for i in distance[1:6]: 
         movies_id=movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
         recommend_poster.append(fetch_poster(movies_id))
@@ -54,9 +32,9 @@ def recommend(movie):
 
 
 
-if st.button("Show Recommend"):
+if st.button("Show Recommendation"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
+    col1,col2,col3,col4,col5,col6,col7,col8,col9,col10=st.columns(10)
     with col1:
         st.text(movie_name[0])
         st.image(movie_poster[0])
@@ -72,5 +50,19 @@ if st.button("Show Recommend"):
     with col5:
         st.text(movie_name[4])
         st.image(movie_poster[4])
-        
+    with col6:
+        st.text(movie_name[5])
+        st.image(movie_poster[5])
+    # with col7:
+    #     st.text(movie_name[6])
+    #     st.image(movie_poster[6])
+    # with col8:
+    #     st.text(movie_name[7])
+    #     st.image(movie_poster[7])
+    # with col9:
+    #     st.text(movie_name[8])
+    #     st.image(movie_poster[8])
+    # with col10:
+    #     st.text(movie_name[9])
+    #     st.image(movie_poster[9])       
         
